145e2fe4802100fe5cb10aacf3eafad9aef4d715
涵曦 <im.hanxi@gmail.com>
2022-10-27 22:44:24 +0800

    update wilder

--- lua/plugins/wilder.lua
+++ lua/plugins/wilder.lua
@@ -3,6 +3,16 @@ wilder.setup({ modes = { ':', '/', '?' } })
 
 wilder.set_option('pipeline', {
   wilder.branch(
+    wilder.python_file_finder_pipeline({
+      -- to use ripgrep : {'rg', '--files'}
+      -- to use fd      : {'fd', '-tf'}
+      file_command = { 'find', '.', '-type', 'f', '-printf', '%P\n' },
+      -- to use fd      : {'fd', '-td'}
+      dir_command = { 'find', '.', '-type', 'd', '-printf', '%P\n' },
+      -- use {'cpsm_filter'} for performance, requires cpsm vim plugin
+      -- found at https://github.com/nixprime/cpsm
+      filters = { 'fuzzy_filter', 'difflib_sorter' },
+    }),
     wilder.cmdline_pipeline({
       -- sets the language to use, 'vim' and 'python' are supported
       language = 'python',
@@ -23,19 +33,8 @@ wilder.set_option('pipeline', {
   ),
 })
 
-wilder.set_option('pipeline', {
-  wilder.branch(
-    wilder.python_file_finder_pipeline({
-      -- to use ripgrep : {'rg', '--files'}
-      -- to use fd      : {'fd', '-tf'}
-      file_command = { 'find', '.', '-type', 'f', '-printf', '%P\n' },
-      -- to use fd      : {'fd', '-td'}
-      dir_command = { 'find', '.', '-type', 'd', '-printf', '%P\n' },
-      -- use {'cpsm_filter'} for performance, requires cpsm vim plugin
-      -- found at https://github.com/nixprime/cpsm
-      filters = { 'fuzzy_filter', 'difflib_sorter' },
-    }),
-    wilder.cmdline_pipeline(),
-    wilder.python_search_pipeline()
-  ),
-})
+wilder.set_option('renderer', wilder.wildmenu_renderer({
+  highlighter = wilder.basic_highlighter(),
+  left = { ' ', wilder.popupmenu_devicons() },
+  right = { ' ', wilder.popupmenu_scrollbar() },
+}))
