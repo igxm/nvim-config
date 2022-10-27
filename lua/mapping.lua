6a202f866980325aa15e655a512151e58aeca68e
涵曦 <im.hanxi@gmail.com>
2022-10-27 22:38:42 +0800

    setup wilder

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -7,12 +7,15 @@ vim.g.mapleader = ','
 -- remove whitespace
 map('n', '<leader><Space>', '<Cmd>StripTrailingWhitespace<CR>')
 
+-- undo toggle
+map('n', '<leader>u', '<Cmd>MundoToggle<CR>')
+
 -- Replace visual selection with text in register, but not contaminate the
 -- register, see also https://stackoverflow.com/q/10723700/6064933.
 map('x', 'p', '"_c<ESC>p')
 
 -- close all plugin window
-map('', '<C-C><C-C>', function () utils.close_plugin_window() end)
+map('', '<C-C><C-C>', function() utils.close_plugin_window() end)
 
 -- close current buffer
 map('', '<C-q>', '<cmd>bp<bar>sp<bar>bn<bar>bd<cr>')
