30ad9dd9f838b86a18e7743063ee15386a07528d
涵曦 <im.hanxi@gmail.com>
2023-04-22 10:49:52 +0800

    add cmdline

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -86,4 +86,34 @@ return {
 			}
 		end,
 	},
+
+	-- cmdline
+	{
+		"hrsh7th/cmp-cmdline",
+		dependencies = {
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-buffer",
+		},
+		config = function()
+			local cmp = require("cmp")
+			-- Use buffer source for `/`.
+			cmp.setup.cmdline('/', {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					-- { name = 'buffer' }
+					{ name = 'buffer', opts = { keyword_pattern = [=[[^[:blank:]].*]=] } }
+				}
+			})
+
+			-- Use cmdline & path source for ':'.
+			cmp.setup.cmdline(':', {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = 'path' }
+				}, {
+					{ name = 'cmdline' }
+				})
+			})
+		end,
+	}
 }
