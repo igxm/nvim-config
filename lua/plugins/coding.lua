9e605c319c3ef101004781475c6a362ac65fa8a0
涵曦 <im.hanxi@gmail.com>
2023-11-17 22:44:00 +0800

    add codeium

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -85,5 +85,12 @@ return {
                 }),
             })
         end,
+		opts = function(_, opts)
+			table.insert(opts.sources, 1, {
+				name = "codeium",
+				group_index = 1,
+				priority = 100,
+			})
+		end,
     },
 }
